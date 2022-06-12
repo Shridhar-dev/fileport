a2049af64272d0f0cd8ae8693bb017953074bb36
Shridhar-dev <shridharkamat10@gmail.com>
2022-06-12 15:57:59 +0530

    added copy/paste feature

--- src/file_porter.rs
+++ src/file_porter.rs
@@ -1,30 +1,56 @@
 use std::fs;
 use ansi_term::Colour;
 
-pub fn port_files(from:&String,to:&String){
+pub fn port_files(from:&String,to:&String,level:u8){
 
     let metadata = fs::metadata(from).unwrap();
     let file_type = metadata.file_type();
     
+    //println!("{:?}",op);
+
     if file_type.is_file(){
         fs::copy(from, to).expect("Error copying files!");
     }
     else{
-        println!("{} {:?}",Colour::Green.paint("Making file:").to_string(),to);
-
+        
+       
         match fs::create_dir_all(to){
             Ok(result) => result,
             Err(_) => println!("Well, there was some issue with the folder!")
         };
 
+        let paths = fs::read_dir(from).unwrap();
+        let filecount = paths.count();
+
+        let mut count = filecount;
+       
         for entry in fs::read_dir(from).unwrap() {
 
             let entry = &entry.unwrap();
             let file_name = entry.file_name().into_string().unwrap();  
             let path = entry.path();
 
+         
+            
+            if level == 0 {
+                print!("{esc}c", esc = 27 as char);
+                println!("\nPorting......\n");
+                println!("{} {:?}",Colour::Green.paint("Making file:").to_string(),path);
+                count = count + 1;
+                let percent = ((count-filecount) as f32 /filecount as f32 )*20 as f32;
+                let roundp = percent.floor() as usize; 
+
+                let repeated = " ".repeat(roundp);
+                let repeated_dash = " ".repeat(20-roundp);
+                println!("🚪{}🎈{}🚪 - {}/{}", repeated, repeated_dash, count-filecount, filecount);
+          
+            }
+
+            
+
             if path.is_dir() {
-                port_files(&path.into_os_string().into_string().unwrap(), &(to.to_owned() + "/" + &file_name));
+                
+                port_files(&path.into_os_string().into_string().unwrap(), &(to.to_owned() + "/" + &file_name),1);
              
             } else {
                 fs::copy(path.into_os_string().into_string().unwrap(), &(to.to_owned() + "/" + &file_name))
--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,21 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    file_porter::port_files(&args[1],&args[2]);
-    
-    match fs::remove_dir_all(&args[1]){
-        Ok(result) => result,
-        Err(_) => println!("Well, there was some issue deleting the folder!")
+    let option = if args.len() > 3 {
+        Some(&args[3])
+    } else {
+        None
     };
+
+    file_porter::port_files(&args[1],&args[2],0);
+
     
-    println!("Done Porting 🚀!");
+    if option != None { 
+        match fs::remove_dir_all(&args[1]){
+            Ok(result) => result,
+            Err(_) => println!("\nWell, there was some issue deleting the folder!\n")
+        };
+    }
+    
+    println!("\nDone Porting 🚀!\n");
 }
